1cb46744142dcdfe4d7a49d23eee780ca6d37950
AyaAhmed123 <aya@tigrisnet.com>
2024-10-17 16:11:31 +0300

    consalidating state logic with a reducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <>
       {/* <PostList /> */}
-      <TodoForm />
-      <TodoList />
+      {/* <TodoForm />
+      <TodoList /> */}
+      <Counter />
     </>
   );
 }
