90822e06743a3438463ac4fd631cedfc4c709cb8
AyaAhmed123 <aya@tigrisnet.com>
2024-10-22 14:00:01 +0300

    prevent unnecessary renders with selector zustand

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -2,13 +2,15 @@ import { create } from "zustand";
 
 interface StoreCounter {
   counter: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
 // set=>({})
 const useStoreCounter = create<StoreCounter>((set) => ({
   counter: 0,
+  max: 5,
   increment: () => set((store) => ({ counter: store.counter + 1 })),
-  reset: () => set(() => ({ counter: 0 })),
+  reset: () => set(() => ({ max: 10 })),
 }));
 export default useStoreCounter;
